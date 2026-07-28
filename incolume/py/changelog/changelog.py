3c0145b518a393958b903cf7d61fe0edbf8f59db
Brito <britodfbr@gmail.com>
2026-07-28 10:28:19 -0300

    refactor: #156 alterado encode na geração do arquivo

--- incolume/py/changelog/changelog.py
+++ incolume/py/changelog/changelog.py
@@ -382,7 +382,9 @@ def changelog_write(
     content_formated = changelog_body(content, content_formated, **kwargs)
     content_formated = changelog_footer(content, content_formated, **kwargs)
 
-    with changelog_file.open('w', encoding='utf-8') as f:
+    content_formated = [x.encode(encoding='utf-8') for x in content_formated]
+
+    with changelog_file.open('wb') as f:
         f.writelines(content_formated)
     return True
 
