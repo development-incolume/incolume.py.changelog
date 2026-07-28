025a22e3d0cc2dac274f6b24da82d1de1f1c14ea
Brito <britodfbr@gmail.com>
2026-07-28 12:01:01 -0300

    refactor: #155 tratativa nova com unidecode para garantir o encode correto

--- incolume/py/changelog/changelog.py
+++ incolume/py/changelog/changelog.py
@@ -384,9 +384,9 @@ def changelog_write(
     content_formated = changelog_body(content, content_formated, **kwargs)
     content_formated = changelog_footer(content, content_formated, **kwargs)
 
-    content_formated = [texto.encode('utf-8').decode('utf-8') for texto in content_formated]
+    content_formated = [texto.encode('utf-8') for texto in content_formated]
 
-    with changelog_file.open('w', encoding='utf-8') as f:
+    with changelog_file.open('wb') as f:
         f.writelines(content_formated)
     return True
 
